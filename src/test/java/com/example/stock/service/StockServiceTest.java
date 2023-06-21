264825935b7fc0bcfbc3f7d22b1c7a1723b1092a
EJ <hanej93@naver.com>
2023-06-21 09:11:22 +0900

    재고감소 로직작성

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,49 @@
+package com.example.stock.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+
+@SpringBootTest
+class StockServiceTest {
+
+	@Autowired
+	private StockService stockService;
+
+	@Autowired
+	private StockRepository stockRepository;
+
+	@BeforeEach
+	void before() {
+		Stock stock = new Stock(1L, 100L);
+
+		stockRepository.saveAndFlush(stock);
+	}
+
+	@AfterEach
+	void after() {
+		stockRepository.deleteAll();
+	}
+
+	@Test
+	void stock_decrease() throws Exception {
+	    // when
+		stockService.decrease(1L, 1L);
+
+	    // then
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		assertThat(99L).isEqualTo(stock.getQuantity());
+	}
+
+
+}
